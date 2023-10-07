562a5eaa1a52abf0ae969f836319b5c8a2aa02e2
unknown <nguyenduykhanh2323@gmail.com>
2023-10-07 10:06:53 +0700

    supply handle print warehouse disable lack data

--- resources/views/orders/users/6/supply_handles/view_handles/print_warehouses/item.blade.php
+++ resources/views/orders/users/6/supply_handles/view_handles/print_warehouses/item.blade.php
@@ -58,7 +58,6 @@
             <p class="font_bold __rest"></p>
         </div>
         <div class="align-items-center mb-2 fs-13" style="display: flex">
-            <input type="hidden" name="c_supply[square][{{ $key_supp }}][{{ $index }}][lack]" value="">
             <label class="mb-0 min_210 text-capitalize text-right mr-3">Vật tư thiếu : </label>
             <p class="color_red font_bold __lack"></p>
         </div>
