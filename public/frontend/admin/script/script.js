d6de63189848194d964d7055d01cf8d885f492a1
owen <nguyenduykhanh2323@gmail.com>
2024-06-19 17:50:26 +0700

    input phone

--- public/frontend/admin/script/script.js
+++ public/frontend/admin/script/script.js
@@ -299,7 +299,7 @@ var selectAjaxModule = function(section = $('.page_content '))
 var phoneInputPrevent = function () {
     $(document).on('keypress paste keydown', 'input[name*=phone]', function (event) {
         let key = event.charCode ? event.charCode : event.keyCode;
-        if (key == 46)
+        if (key !== 8 && (key === 32 || key < 48 || (key > 57 && key < 65) || (key > 90 && key < 97) || key > 122))
         {
             event.preventDefault();
             return false;
