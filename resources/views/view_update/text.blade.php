a1136c40452af8a1614aa81632f43dd13626636a
ơen <nguyenduykhanh2323@gmail.com>
2024-09-07 16:32:24 +0700

    enable input negative number in compent plus input

--- resources/views/view_update/text.blade.php
+++ resources/views/view_update/text.blade.php
@@ -16,7 +16,7 @@
     class="form-control{{ @$attr['inject_class'] ? ' '.$attr['inject_class'] : '' }}" name="{{ $name }}" value="{{ @$value }}"
     {{ @$attr['disable_field'] == 1 || ($check_disabled) ? 'disabled' : '' }}
     {{ @$attr['readonly'] == 1 || (@$attr['readonly'] == 2 && !empty($value)) ? 'readonly' : '' }}
-    {{ $type_input == 'number' ? 'min=0 step=any' : '' }} 
+    {{ $type_input == 'number' ? ' step=any' : '' }} 
     placeholder="{{ @$attr['placeholder'] ?? 'Nhập '.mb_strtolower($note) }}">
     @if ($check_disabled)
         <button type="button" class="p-2 bg_green color_white box_shadow_3 ml-2 __pass_change"><i class="fa fa-pencil-square-o mr-1" aria-hidden="true"></i></button>    
