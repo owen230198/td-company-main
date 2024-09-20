e09ea373b33311ad135cd6d562c8e785dfa09155
ơen <nguyenduykhanh2323@gmail.com>
2024-09-20 23:22:12 +0700

    add represent for accounting

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -595,7 +595,7 @@ class AdminController extends Controller
 
     public function addLinkingData(Request $request)
     {
-        if (\GroupUser::isAdmin() || \GroupUser::isSale()) {
+        if (\GroupUser::isAdmin() || \GroupUser::isSale() || \GroupUser::isAccounting()) {
             return view('customers.item_represent', ['index' => (int) $request->input('index')]);
         }else{
             return returnMessageAjax(100, 'Bạn không có quyền thao tác !');
