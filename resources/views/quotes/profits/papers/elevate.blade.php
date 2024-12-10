204afc9c0b71b380fdc5a71e2cb5efe41bc5b978
ơen <nguyenduykhanh2323@gmail.com>
2024-12-10 14:09:38 +0700

    fix bug undf float qty in profit view

--- resources/views/quotes/profits/papers/elevate.blade.php
+++ resources/views/quotes/profits/papers/elevate.blade.php
@@ -119,7 +119,7 @@
     <p class="fs-15 font_bold">Tổng chi phí cho máy bế: (1) + (2) + (3) + (4) + Giá thêm cho khuôn phức tạp (nếu có) = {{ number_format($stage['cost']) }}đ</p>       
 </div>
 
-@if (!empty($float['act']))
+@if (!empty($float['act']) && !empty($float['qty_pro']))
     <div class="mt-2 pt-2 border_top_thin formula_tab">
         @if (\GroupUser::isAdmin())
             <p class="fs-15 color_green mb-2 font_bold">Công Thức Tính Chi Phí Thúc Nổi</p>
